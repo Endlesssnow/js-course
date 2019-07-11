85e1f33789f2ad815e9c18757aa62f07643fb764
Alex Yerokhin <alexandr.yerokhin.23@gmail.com>
2019-07-11 17:25:59 +0300

    first commit

---
+++ lecture_2/homework.js
@@ -0,0 +1,35 @@
+function drawTriangle(){
+	var height = prompt('Введите высоту: ');
+	var str = '';
+		for (var i = 0; i < height - 1; i++){
+			str += ' '.repeat(height - 1 - i) + '/' + ' '.repeat(2 * i) + '\\\n';
+		}
+		str += ' '.repeat(height - 1 - i) + '/' + '_'.repeat(2 * i) + '\\\n';
+		console.log(str);
+}
+
+
+function drawRomb(){
+	var height = prompt('Введите высоту: ');
+	var str = '';
+		for (var i = 0; i < height; i++){
+			str += ' '.repeat(height - 1 - i) + '/' + ' '.repeat(2 * i) + '\\\n';
+		}
+		for (var j = height; j > 0; j--){
+			str += ' '.repeat(height - j) + '\\' + ' '.repeat(2 * (j-1)) + '/\n';
+		}
+		console.log(str);
+}
+
+
+function multiply(num1,num2){
+	var multiply;
+	multiply = 0;
+	for(i = 0; i < Math.abs(num2); i++){
+		multiply += num1;
+  	}
+  	if (num2 < 0){
+  		multiply *= -1;
+  	}
+  console.log(multiply);
+}
