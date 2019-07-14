9eae92a1fe4dda0e10431f72d80d089f0d079ad5
Alex Yerokhin <alexandr.yerokhin.23@gmail.com>
2019-07-14 16:01:53 +0300

    null index2

--- lecture_3/toggledrowdown.js
+++ lecture_3/toggledrowdown.js
@@ -1,24 +1,51 @@
-var menu = document.querySelector('#menu');
+var fragToggleDD = document.createDocumentFragment();
 
-var home = document.querySelector('#home');
-var feed = document.querySelector('#feed');
-var store = document.querySelector('#store');
+var sectionToggleDD = document.createElement('section');
+sectionToggleDD.id = 'section';
+
+var menuToggleDD = document.createElement('div');
+menuToggleDD.id = 'menu';
+
+var containerToggleDD = document.createElement('div');
+containerToggleDD.id = 'container';
+	var actionToggleDD = document.createElement('div');
+	actionToggleDD.id = 'action';
+	var anotherActionToggleDD = document.createElement('div');
+	anotherActionToggleDD.id = 'anotherAction';
+	var mayBeActionToggleDD = document.createElement('div');
+	mayBeActionToggleDD.id = 'mayBeAction';
+
+fragToggleDD.appendChild(sectionToggleDD);
+sectionToggleDD.appendChild(menuToggleDD);
+sectionToggleDD.appendChild(containerToggleDD);
+containerToggleDD.appendChild(actionToggleDD);
+containerToggleDD.appendChild(anotherActionToggleDD);
+containerToggleDD.appendChild(mayBeActionToggleDD);
+
+document.body.appendChild(fragToggleDD);
+
+var menuToggleDD = document.querySelector('#menu');
+
+var actionToggleDD = document.querySelector('#action');
+var anotherActionToggleDD = document.querySelector('#anotherAction');
+var store = document.querySelector('#mayBeAction');
 
 var click = 0;
 
-home.style.display = 'none';
-feed.style.display = 'none';
-store.style.display = 'none';
-menu.addEventListener('click', function(){
+actionToggleDD.style.display = 'none';
+anotherActionToggleDD.style.display = 'none';
+mayBeActionToggleDD.style.display = 'none';
+
+menuToggleDD.addEventListener('click', function(){
 if (click === 1){
-	home.style.display = 'none';
-	feed.style.display = 'none';
-	store.style.display = 'none';
+	actionToggleDD.style.display = 'none';
+	anotherActionToggleDD.style.display = 'none';
+	mayBeActionToggleDD.style.display = 'none';
 	click = 0;
 }else {
-	home.style.display = 'block';
-	feed.style.display = 'block';
-	store.style.display = 'block';
+	actionToggleDD.style.display = 'block';
+	anotherActionToggleDD.style.display = 'block';
+	mayBeActionToggleDD.style.display = 'block';
 	click = 1;
 	}
 })
