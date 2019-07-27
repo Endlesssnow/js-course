e8b84f34ff3a7ea2d6d1d9843289fe19112e52d1
Alex Yerokhin <alexandr.yerokhin.23@gmail.com>
2019-07-27 19:45:04 +0300

    New

--- lecture_6/index.js
+++ lecture_6/index.js
@@ -10,63 +10,39 @@ var routes = {
 	}
 };
 
-var requestTemplate = (function () {
-	var cache = {};
-	return async function(url){
-		if(cache.hasOwnProperty(url)){
-			return Promise.resolve(cache[url]);
-		} else {
-			const res = await fetch(url);
-            const html = await res.text();
-            cache[url] = html;
-            return html;
-		}
-	}
+var requestTemplate = (function(){
+    var cache = {};
+    return function(url){
+        if(cache.hasOwnProperty(url)){
+            return Promise.resolve(cache[url]);
+        } else {
+            return fetch(url).then(function(res){
+                return res.text();
+            }).then(function(html){
+                cache[url] = html;
+                return html;
+            })
+        }
+    }
+})
+
+var heandleRouting = (function () {
+    var previousHash;
+    return function () {
+        var hash = window.location.hash.split('#/')[1] || '';
+        if (previousHash === hash){
+            return;
+        }
+        if(routes.hasOwnProperty(hash)){
+            previousHash = hash;
+            var url = routes[hash];
+            console.log(routes[hash]);
+            requestTemplate(url).then( function (html){
+                console.log(html);
+            })
+        }
+    }    
 })();
 
-var runScript = (function () {
-	var cache = {};
-
-	return function (src) {
-		if(cache.hasOwnProperty(src)){
-			cache[src]();
-		} else {
-			import(src).then(function (module) {
-				cache[src] = module.default;
-				cache[src]();
-			}).catch( function (err) {
-				console.error(err);
-			})
-		}
-	}
-})();
-
-var render = (function(){
-	var content = document.getElementById('content');
-	return function(html){
-		content.innerHTML = html;
-	}
-})();
-
-var handleRouting = (function(){
-	var previousHash;
-	return function(){
-		var hash = window.location.hash.split('#/')[1] || '';
-		if(previousHash === hash){
-			return;
-		}
-		if(routes.hasOwnProperty(hash)){
-			previousHash = hash;
-			var urls = routes[hash];
-			requestTemplate(urls.html).then( function (html) {
-				render(html);
-				if(urls.hasOwnProperty('src')){
-					runScript(urls.src);
-				}
-			})
-		}
-	}
-})();
-
-window.addEventListener('DOMContentLoaded', handleRouting);
-window.addEventListener('hashchange', handleRouting);
+window.addEventListener('DOMContentLoader', heandleRouting);
+window.addEventListener('hashchange', heandleRouting);
