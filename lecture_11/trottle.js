c0c0ff296527ab43a6e94309c1784e330a70e86a
Alex Yerokhin <alexandr.yerokhin.23@gmail.com>
2019-08-20 18:52:02 +0300

    New

--- lecture_11/trottle.js
+++ lecture_11/trottle.js
@@ -42,21 +42,6 @@ const throttleUpdate = throttle(work, 1000);
 addEventListener('mousemove', throttleUpdate);
 
 
-function debounce(func, wait, immediate) {
-	let timeout;
-	return function() {
-		let context = this, args = arguments;
-		clearTimeout(timeout);
-		timeout = setTimeout(function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		}, wait);
-		if (immediate && !timeout) func.apply(context, args);
-	};
-}
-
-addEventListener('click', debounce);
-
 function intersect(a, b) {
   let result = [];
 
