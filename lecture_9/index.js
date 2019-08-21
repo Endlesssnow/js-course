a28fd6cb8e8c5323ecff5de7f4621d596f917ca5
Alex Yerokhin <alexandr.yerokhin.23@gmail.com>
2019-08-21 16:45:50 +0300

    new

--- lecture_9/index.js
+++ lecture_9/index.js
@@ -34,81 +34,59 @@ class Geolocation {
         console.log(position);
     }
     getPos(){
+        return new Promise((resolve, _reject) => {
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(Geolocation.getPos);
         }
-    }  
+        resolve();
+        }
+    )}  
 
     static watchPos(position) {
         console.log(position);
     }
     watchPos(){
+        return new Promise((resolve, _reject) => {
         if(navigator.geolocation){
             navigator.geolocation.watchPosition(Geolocation.watchPos);
         }
-    } 
+        resolve();
+        } 
+    )}
 }
 
-class List {
-    constructor(listDiv) {
-        if (listDiv === undefined) {
-            listDiv = document.body;
-        } else {
-            listDiv = document.querySelector(listDiv);
+class Media {
+
+    constructor(selector, audio = false, video = true) {
+        this.selector = selector;
+        this.constraints = {
+            audio: audio,
+            video: video
         }
-        const fragment = document.createDocumentFragment();
-        this.list = document.createElement('ul');
-        this.input = document.createElement('input');
-        this.input.placeholder = 'Add list item';
-        fragment.appendChild(this.input);
-        fragment.appendChild(this.list);
-        listDiv.appendChild(fragment);
-
-        this.input.addEventListener('keydown', (event) => {
-            if (event.key == 'Enter') {
-                this.addItem(this.input.value);
-                this.input.value = '';
-            }
-        })
-
-        this.counter = 0;
-        for (let i = 0, len = localStorage.length, index; i < len; ++i) {
-            if (localStorage.key(i).includes('listItem')) {
-                index = localStorage.key(i);
-                const listItem = document.createElement('li');
-                listItem.innerText = localStorage.getItem(index);
-                this.list.appendChild(listItem);
-                if(index.slice('listItem'.length) > this.counter + 1) this.counter = +index.slice('listItem'.length) + 1;
-                console.log(index.slice('listItem'.length));
-            };
-
-        }
+        this.video = document.createElement('video');
+        this.video.width = 640;
+        this.video.height = 480;
     }
 
-    addItem(item) {
-        localStorage.setItem('listItem' + this.counter, item);
-        const listItem = document.createElement('li');
-        listItem.innerText = item;
-
-        this.list.appendChild(listItem);
-        this.counter++;
-    }
-
-    attachTo(block) {
-        if (block !== undefined) {
-            block.appendChild(this.input);
-            block.body.appendChild(this.list);
-        }
-    }
-
-    attachBy(selector) {
-        if (selector !== undefined) {
-            const block = document.querySelector(selector);
-            block.appendChild(this.input);
-            block.body.appendChild(this.list);
-        }
+    getMedia() {
+        return new Promise((resolve, reject) => {
+            let media;
+            const selector = this.selector;
+            const video = this.video;
+            navigator.mediaDevices.getUserMedia(this.constraints)
+                .then((stream) => {
+                    if (selector === undefined) media = document.body;
+                    else media = document.querySelector(selector);
+                    media.appendChild(video);
+                    video.style.display = 'block';
+                    video.srcObject = stream;
+                    video.onloadedmetadata = () => video.play();
+                    resolve()
+                })
+                .catch((err) => {
+                    console.log(err);
+                    reject();
+                });
+        });
     }
 }
-
-
-const list = new List('#listDiv');
